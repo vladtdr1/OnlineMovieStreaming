ab3275a6f082bef2300625e7c17ed92e2e5e7219
vladtdr1 <vladtdr1@gmail.com>
2020-05-15 17:01:22 +0300

    OMS-18 Created javafx login GUI

---
+++ src/sample.Main/java/Controllers/Controller.java
@@ -0,0 +1,18 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class Controller {
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private Text loginResponse;
+
+    public void handleLoginButton(ActionEvent actionEvent) {
+    }
+}
