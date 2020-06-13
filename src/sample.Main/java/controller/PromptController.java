dbe00f7bf0a6517df2b3c949cc40982bde15e8ce
vladtdr1 <vladtdr1@gmail.com>
2020-06-13 17:46:35 +0300

    Delete button actually deletes user

--- src/sample.Main/java/controller/PromptController.java
+++ src/sample.Main/java/controller/PromptController.java
@@ -1,14 +1,32 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.User;
+import service.UserService;
+
+import java.io.IOException;
 
 public class PromptController {
     public void handleYesButton(ActionEvent actionEvent) {
         User u = AdminController.getSelectedUser();
-        System.out.println(u + " shall be deleted");
+        UserService.removeUser(u);
+
+        Parent p;
+        try { //this try-catch makes sure the users table gets updated by reopening it
+            p = FXMLLoader.load(getClass().getResource("/fxml/admin.fxml"));
+            Scene scene=new Scene(p,600,400);
+            Stage window1=AdminController.getStage();
+            window1.setScene(scene);
+            window1.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         Stage window=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
         window.close();
     }
