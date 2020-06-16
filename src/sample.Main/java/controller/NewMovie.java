14d463ed683b9e8ccca04f4bbe95e67c4283c919
vladtdr1 <vladtdr1@gmail.com>
2020-06-17 00:43:43 +0300

    OMS-27 Created ADD MOVIE GUI and reorder some javafx elements

--- src/sample.Main/java/controller/NewMovie.java
+++ src/sample.Main/java/controller/NewMovie.java
@@ -10,30 +10,35 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
+import static service.MovieService.addMovie;
 import static service.UserService.addUser;
+import static service.UserService.getConnectedUser;
 
 
-public class NewUser {
+public class NewMovie {
     @FXML
-    private TextField usernameText;
+    private TextField urlText;
     @FXML
-    private TextField passwordText;
+    private TextField yearText;
     @FXML
-    private TextField emailText;
+    private TextField titleText;
+    @FXML
+    private TextArea descriptionText;
     @FXML
     private Text registrationResponse;
     @FXML
-    private ChoiceBox roleText;
+    private ChoiceBox genreText;
 
     public void cancelButton(ActionEvent actionEvent) {
         try {
-            Parent p= FXMLLoader.load(getClass().getResource("/fxml/admin.fxml"));
+            Parent p= FXMLLoader.load(getClass().getResource("/fxml/uploader.fxml"));
             Scene scene=new Scene(p,600,400);
             Stage window=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
             window.setScene(scene);
@@ -46,8 +51,8 @@ public class NewUser {
 
     public void okButton(ActionEvent actionEvent) {
         try {
-            addUser(usernameText.getText(),passwordText.getText(), emailText.getText(), (String) roleText.getValue());
-            Parent p= FXMLLoader.load(getClass().getResource("/fxml/admin.fxml"));
+            addMovie(titleText.getText(),urlText.getText(),(String)genreText.getValue(),descriptionText.getText(),getConnectedUser().getUsername(),yearText.getText());
+            Parent p= FXMLLoader.load(getClass().getResource("/fxml/uploader.fxml"));
             Scene scene=new Scene(p,600,400);
             Stage window=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
             window.setScene(scene);
@@ -55,7 +60,7 @@ public class NewUser {
 
         }catch(IOException e) {
             e.printStackTrace();
-        } catch (PasswordFieldEmptyException |UsernameFieldEmptyException |UsernameAlreadyExistsException e ) {
+        } catch (Exception e ) {
             registrationResponse.setText(e.getMessage());
         }
     }
--- src/sample.Main/java/controller/UploaderController.java
+++ src/sample.Main/java/controller/UploaderController.java
@@ -79,5 +79,15 @@ public class UploaderController implements Initializable {
     }
 
     public void handleAddMovieButton(ActionEvent actionEvent) {
+        try {
+            Parent p= FXMLLoader.load(getClass().getResource("/fxml/newMovie.fxml"));
+            Scene scene=new Scene(p,600,400);
+            Stage window=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+            window.setScene(scene);
+            window.show();
+        }catch(IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
