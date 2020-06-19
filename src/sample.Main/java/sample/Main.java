257876ea6f3cd01ae2c442b4e646e76e2878ddce
OanaVasilescu <vasilescu.oana28@gmail.com>
2020-06-19 22:20:45 +0300

    OSM-29 Made the interface dark

--- src/sample.Main/java/sample/Main.java
+++ src/sample.Main/java/sample/Main.java
@@ -20,9 +20,12 @@ public class Main extends Application {
         /*addUser("viewer","viewer","viewer@viewrw.viewer","viewer);
         addUser("uploader","uploader","uploader@uploader.uploader","uploader);
         addUser("admin","admin","admin@admin.admin","admin);*///(added placeholder accs)
+
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
         primaryStage.setTitle("Movie Streaming Platform login");
-        primaryStage.setScene(new Scene(root, 500, 300));
+        Scene scene = new Scene(root, 500, 300);
+        scene.getStylesheets().add("Style.css");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
