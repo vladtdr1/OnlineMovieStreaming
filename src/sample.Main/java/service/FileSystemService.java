6f00bc2cc1e52059df9d38bf822517e0ce3d969a
OanaVasilescu <vasilescu.oana28@gmail.com>
2020-06-20 20:01:17 +0300

    OSM-51 moves json files to user.home kept json files filled as template

---
+++ src/sample.Main/java/service/FileSystemService.java
@@ -0,0 +1,12 @@
+package service;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileSystemService {
+    private static final String APPLICATION_FOLDER = "FletNix";
+    private static final String USER_FOLDER = System.getProperty("user.home");
+    public static final Path APPLICATION_HOME_PATH = Paths.get(USER_FOLDER, APPLICATION_FOLDER);
+    public static Path getPathToFile(String... path) {
+        return APPLICATION_HOME_PATH.resolve(Paths.get("", path));
+    }
+}
