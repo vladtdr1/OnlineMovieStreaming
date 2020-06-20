2667d38e3b8a4f9602244a1dd8c9c72a0bfd724a
vladtdr1 <vladtdr1@gmail.com>
2020-06-21 00:11:05 +0300

    OMS-53 Added login tests

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -2,6 +2,7 @@ package controller;
 
 import exception.IncorrectPasswordException;
 import exception.UserDoesNotExistException;
+import exception.UsernameFieldEmptyException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -20,45 +21,51 @@ import static service.UserService.*;
 
 public class LoginController {
     @FXML
-    private TextField usernameField;
+    public TextField usernameField;
     @FXML
-    private PasswordField passwordField;
+    public PasswordField passwordField;
     @FXML
-    private Text loginResponse;
+    public Text loginResponse;
 
     public void onEnter(ActionEvent ae){
         handleLoginButton(ae);
     }
 
     public void handleLoginButton(ActionEvent actionEvent) {
+        tryLogin((Stage)((Node)actionEvent.getSource()).getScene().getWindow());
+
+    }
+
+    void tryLogin(Stage stage)
+    {
+        setConnectedUser(null);
         try {
             setConnectedUser(checkUser(usernameField.getText(), passwordField.getText()));
+            if(stage==null)
+                return;
             if(getConnectedUser().getRole().equals("viewer"))
             {//viewer
                 Parent p= FXMLLoader.load(getClass().getResource("/fxml/viewer.fxml"));
                 Scene scene=new Scene(p,600,400);
-                Stage window=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-                window.setTitle("FletNix viewer");
-                window.setScene(scene);
-                window.show();
+                stage.setTitle("FletNix viewer");
+                stage.setScene(scene);
+                stage.show();
             }
             if(getConnectedUser().getRole().equals("uploader"))
             {//uploader
                 Parent p= FXMLLoader.load(getClass().getResource("/fxml/uploader.fxml"));
                 Scene scene=new Scene(p,600,400);
-                Stage window=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-                window.setTitle("FletNix uploader");
-                window.setScene(scene);
-                window.show();
+                stage.setTitle("FletNix uploader");
+                stage.setScene(scene);
+                stage.show();
             }
             if(getConnectedUser().getRole().equals("admin"))
             {//admin
                 Parent p= FXMLLoader.load(getClass().getResource("/fxml/admin.fxml"));
                 Scene scene=new Scene(p,600,400);
-                Stage window=(Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-                window.setTitle("FletNix admin");
-                window.setScene(scene);
-                window.show();
+                stage.setTitle("FletNix admin");
+                stage.setScene(scene);
+                stage.show();
             }
         }catch(UserDoesNotExistException | IncorrectPasswordException | IOException e) {
             loginResponse.setText(e.getMessage());
